03417396013f932e0bf811a9852e4472d1e66d7e
Crayon <lsh328328@naver.com>
2022-11-13 08:06:31 +0900

    [Feat]: chat model 구현
    chat model 구현
    chat repository 세팅
    
    Related to: #1

--- src/main/java/com/sopt31th/server/domain/music/MusicRepository.java
+++ src/main/java/com/sopt31th/server/domain/music/MusicRepository.java
@@ -3,5 +3,5 @@ package com.sopt31th.server.domain.music;
 import com.sopt31th.server.domain.music.repository.MusicRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MusicRepository extends JpaRepository<Long, Music>, MusicRepositoryCustom {
+public interface MusicRepository extends JpaRepository<Music, Long>, MusicRepositoryCustom {
 }
