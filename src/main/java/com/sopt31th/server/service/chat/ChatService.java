75d88525b944bd9b5a61a67971151c1d02c1af00
Crayon <lsh328328@naver.com>
2022-11-15 23:45:44 +0900

    [Update]: 채팅 response 값 수정

--- src/main/java/com/sopt31th/server/service/chat/ChatService.java
+++ src/main/java/com/sopt31th/server/service/chat/ChatService.java
@@ -1,33 +1,37 @@
 package com.sopt31th.server.service.chat;
 
+import com.sopt31th.server.controller.chat.dto.response.ChatHeaderResponse;
 import com.sopt31th.server.controller.chat.dto.response.ChatResponse;
 import com.sopt31th.server.domain.chat.Chat;
 import com.sopt31th.server.domain.chat.ChatRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.validation.constraints.NotNull;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
 public class ChatService {
 
     private final ChatRepository chatRepository;
+    @Value("${image.header}")
+    String imageUrl;
 
-    public List<ChatResponse> getList() {
+    public ChatResponse getList() {
         return getChatResponseList(chatRepository.findAll());
     }
 
     @NotNull
-    private List<ChatResponse> getChatResponseList(final List<Chat> chatList) {
-        return chatList.stream()
-                .map(c -> ChatResponse.of(
-                        c.getId(),
-                        c.getChatImage(),
-                        c.getName(),
-                        c.getContent()
-                )).collect(Collectors.toList());
+    private ChatResponse getChatResponseList(final List<Chat> chatList) {
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        ChatHeaderResponse[] content = new ChatHeaderResponse[chatList.size()];
+        for (int c = 0; c < content.length; c++) {
+            Chat chat = chatList.get(c);
+            content[c] = new ChatHeaderResponse(chat.getChatImage(), chat.getName(), chat.getContent(), chat.getUpdatedAt().format(format));
+        }
+        return new ChatResponse(imageUrl, content);
     }
 }
